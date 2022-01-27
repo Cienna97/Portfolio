cc71e474980aea357ad092b406202392d242029a
Cienna97 <cmortenson97@gmail.com>
2022-01-26 21:25:43 -0700

    working on importing thru header file

--- my-app/src/Component/Header/index.js
+++ my-app/src/Component/Header/index.js
@@ -1,2 +1,34 @@
+import React, { Component } from "react";
+import { Route, HashRouter , Routes} from "react-router-dom";
+//import { Redirect } from 'react-router'
+import Nav from "./Nav";
+import About from "./About";
+import Portfolio from "./Portfolio";
+import Contact from './Contact';
 
+class Header extends Component {
+    render() {
+        return (
+            <HashRouter>
+                <header>
+                    <hr/>
+                    <Nav/>
+                    <hr/>
+                </header>
+
+                <div classname="content">
+                <Routes>
+                <Route exact path="/" element={ <About />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/portfolio" element={<Portfolio />} /> 
+                </Routes>
+
+                </div>
+            </HashRouter>
+        );
+    }
+}
+
+export default Header;
 
