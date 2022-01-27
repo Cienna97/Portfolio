a7ec4b387cb0747e4690a231096d355a006590ed
Cienna97 <cmortenson97@gmail.com>
2022-01-26 22:24:11 -0700

    eliminating files for test, HEADER, node packages, packages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import './App.css';
-import Header from './Component/Header';
+//import Header from './Component/Header';
 import Footer from './Component/Footer';
 //import Nav from './Component/Nav';
-import About from './Component/About';
 //import Contact from "./Component/Contact";
-import Projects from "./Component/Projects";
 
 //import logo from './logo.svg';
 
 function App() {
   return (
    <div>
-    <main>
-      <Projects></Projects>
-        <About></About>
-        <h1>
-          <Header></Header>
-          <Footer/>
-        </h1>
-      </main>
+    <h1 id="myname">
+    Cienna Mortenson
+    </h1>
+    <Footer />
       
     </div>
   );
--- my-app/src/Component/Work/index.js
+++ my-app/src/Component/Work/index.js
@@ -1,2 +1,57 @@
 import React from "react";
 
+function Work(props) {
+    return (
+        <div>
+             {props.projects.map((project) => (
+                <div  className='whole-card'>
+                    <div>
+                    <p className="title" key={project.id}>
+                                    {project.title}
+                                </p>
+                        <figure>
+                            <a href={project.live} target="_blank" rel="noreferrer">
+                                <img className="img"src={process.env.PUBLIC_URL + project.image} alt="Placeholder image" />
+                            </a>
+                        </figure>
+                    </div>
+                    <div className="card-content">
+                        <div className="media">
+                            <div className="media-left"></div>
+                            <div className="media-content">
+                               
+                            </div>
+                        </div>
+
+                        <div>
+                            {project.description}
+                            <div className="card">
+                                <div>
+                                    <a
+                                        href={project.repo}
+                                        className="card-footer-item"
+                                        target="_blank" rel="noreferrer"
+                                    >
+                                        Repo
+                                    </a>
+                                    <a>|</a>
+                                    <a
+                                        href={project.live}
+                                        className="card-footer-item"
+                                        target="_blank" rel="noreferrer"
+                                    >
+                                        Live Site
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            ))}
+
+        </div>
+    );
+}
+
+export default Work;
